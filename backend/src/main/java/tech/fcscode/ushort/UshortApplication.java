08f6654fa450dac397aea9d6a1f5ec64cb9c2b78
frankcs96 <fcsuarez96@gmail.com>
2020-05-27 15:09:09 +0200

    Creating project

---
+++ backend/src/main/java/tech/fcscode/ushort/UshortApplication.java
@@ -0,0 +1,13 @@
+package tech.fcscode.ushort;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UshortApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(UshortApplication.class, args);
+  }
+
+}
---
+++ backend/src/test/java/tech/fcscode/ushort/UshortApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.fcscode.ushort;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UshortApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
