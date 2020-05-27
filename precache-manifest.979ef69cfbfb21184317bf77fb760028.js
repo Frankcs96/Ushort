f8e95b3fd5a0393f1e0f521e96a53403e256d3ec
frankcs96 <fcsuarez96@gmail.com>
2020-05-27 15:17:39 +0200

    Updates

---
+++ precache-manifest.979ef69cfbfb21184317bf77fb760028.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5751395b7b07d59b96529d02582002f6",
+    "url": "/Ushort/index.html"
+  },
+  {
+    "revision": "8216bc2e901041b77c65",
+    "url": "/Ushort/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2bd18b008f62c42e5cf7",
+    "url": "/Ushort/static/js/2.e131b932.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Ushort/static/js/2.e131b932.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8216bc2e901041b77c65",
+    "url": "/Ushort/static/js/main.9a0949b9.chunk.js"
+  },
+  {
+    "revision": "f03d87d49757c8c41266",
+    "url": "/Ushort/static/js/runtime-main.89a72c52.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Ushort/static/media/logo.5d5d9eef.svg"
+  }
+]);
