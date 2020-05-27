39c41ef504e6ecfb4d330065acef11e5c1ae9a17
frankcs96 <fcsuarez96@gmail.com>
2020-05-27 15:40:39 +0200

    Creating project

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          TESTING APP XD
         </a>
       </header>
     </div>
