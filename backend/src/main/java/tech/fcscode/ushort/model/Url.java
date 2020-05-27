583c6503f718384afdeb6af37e3157eefd52b5a8
frankcs96 <fcsuarez96@gmail.com>
2020-05-27 18:52:47 +0200

    Creating url model

---
+++ backend/src/main/java/tech/fcscode/ushort/model/Url.java
@@ -0,0 +1,72 @@
+package tech.fcscode.ushort.model;
+
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import org.hibernate.validator.constraints.URL;
+
+@Entity
+@Table(name = "url")
+public class Url {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private long id;
+
+  @Column(nullable = false)
+  @URL
+  private String longUrl;
+
+  @Column(nullable = false)
+  private String shortUrl;
+
+  @Column(nullable = false)
+  private Date createdDate;
+
+  @Column(nullable = false)
+  private Date expirationDate;
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public String getLongUrl() {
+    return longUrl;
+  }
+
+  public void setLongUrl(String longUrl) {
+    this.longUrl = longUrl;
+  }
+
+  public String getShortUrl() {
+    return shortUrl;
+  }
+
+  public void setShortUrl(String shortUrl) {
+    this.shortUrl = shortUrl;
+  }
+
+  public Date getCreatedDate() {
+    return createdDate;
+  }
+
+  public void setCreatedDate(Date createdDate) {
+    this.createdDate = createdDate;
+  }
+
+  public Date getExpirationDate() {
+    return expirationDate;
+  }
+
+  public void setExpirationDate(Date expirationDate) {
+    this.expirationDate = expirationDate;
+  }
+}
